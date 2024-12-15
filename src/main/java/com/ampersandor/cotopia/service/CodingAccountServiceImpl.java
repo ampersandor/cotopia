489f6dc7c44304214ff9efb6c45e7dcd4174a1fa
DongHunKim <ddong3525@gmail.com>
2024-12-15 22:35:42 +0900

    bug: add Transactional annotation in Coding Account Service

--- src/main/java/com/ampersandor/cotopia/service/CodingAccountServiceImpl.java
+++ src/main/java/com/ampersandor/cotopia/service/CodingAccountServiceImpl.java
@@ -5,6 +5,8 @@ import com.ampersandor.cotopia.repository.CodingAccountRepository;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class CodingAccountServiceImpl implements CodingAccountService {
 
     private final CodingAccountRepository codingAccountRepository;
@@ -14,16 +16,19 @@ public class CodingAccountServiceImpl implements CodingAccountService {
     }
 
     @Override
+    @Transactional
     public CodingAccount save(CodingAccount codingAccount) {
         return codingAccountRepository.save(codingAccount);
     }
 
     @Override
+    @Transactional
     public void delete(Long codingAccountId) {
         codingAccountRepository.delete(codingAccountId);
     }
 
     @Override
+    @Transactional
     public CodingAccount update(CodingAccount codingAccount) {
         return codingAccountRepository.update(codingAccount);
     }

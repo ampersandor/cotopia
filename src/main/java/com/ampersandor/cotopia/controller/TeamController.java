5ad2d2743dc01558fb4bba77db4e99c8eadb19a5
DongHunKim <ddong3525@gmail.com>
2024-12-15 22:25:56 +0900

    feature: add getFoodsByTeam funciton in Team Controller

--- src/main/java/com/ampersandor/cotopia/controller/TeamController.java
+++ src/main/java/com/ampersandor/cotopia/controller/TeamController.java
@@ -2,10 +2,12 @@ package com.ampersandor.cotopia.controller;
 
 import com.ampersandor.cotopia.common.MyLogger;
 import com.ampersandor.cotopia.dto.TeamDTO;
+import com.ampersandor.cotopia.dto.FoodDTO;
 import com.ampersandor.cotopia.entity.Team;
 import com.ampersandor.cotopia.service.TeamService;
 import com.ampersandor.cotopia.service.UserService;
 import com.ampersandor.cotopia.service.AuthService;
+import com.ampersandor.cotopia.service.FoodService;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +28,7 @@ public class TeamController {
     private final TeamService teamService;
     private final UserService userService;
     private final AuthService authService;
+    private final FoodService foodService;
     private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @GetMapping("")
@@ -57,30 +61,39 @@ public class TeamController {
     }
 
     @PutMapping("/{teamId}")
-    public ResponseEntity<TeamDTO.Response> updateTeam(@PathVariable Long teamId, @RequestBody TeamDTO.UpdateRequest teamRequest, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
+    public ResponseEntity<TeamDTO.Response> updateTeam(@PathVariable("teamId") Long teamId, @RequestBody TeamDTO.UpdateRequest teamRequest, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
         Long userId = authService.getUserIdFromToken(token);
         Team updatedTeam = teamService.updateTeam(teamRequest.toEntity(), userId);
         return ResponseEntity.ok(TeamDTO.Response.from(updatedTeam));
     }
 
     @DeleteMapping("/{teamId}")
-    public ResponseEntity<Void> deleteTeam(@PathVariable Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
+    public ResponseEntity<Void> deleteTeam(@PathVariable("teamId") Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
         Long userId = authService.getUserIdFromToken(token);
         teamService.deleteTeam(teamId, userId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{teamId}/leave")
-    public ResponseEntity<Void> leaveTeam(@PathVariable Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
+    public ResponseEntity<Void> leaveTeam(@PathVariable("teamId") Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
         Long userId = authService.getUserIdFromToken(token);
         teamService.leaveTeam(teamId, userId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{teamId}/join")
-    public ResponseEntity<Void> joinTeam(@PathVariable Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
+    public ResponseEntity<Void> joinTeam(@PathVariable("teamId") Long teamId, @CookieValue(name = "jwt") String token, HttpServletRequest request) {
         Long userId = authService.getUserIdFromToken(token);
         teamService.joinTeam(teamId, userId);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{teamId}/foods")
+    public ResponseEntity<List<FoodDTO.Response>> getFoodsByTeamId(@PathVariable("teamId") Long teamId, HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(request.getRequestURI());
+        return ResponseEntity.ok(foodService.getFoodsByTeamId(teamId, LocalDate.now()).stream().map(FoodDTO.Response::from).collect(Collectors.toList()));
+    }
+
+
 }
